310cc3e2b729ec3a02718e2116493e9bc76be591
nameis-uttam <uttamjena343@gmail.com>
2023-12-02 16:17:27 +0530

    adding animation

--- js/script.js
+++ js/script.js
@@ -28,6 +28,12 @@ window.onscroll = () => {
           .querySelector("header nav a[href*=" + id + "]")
           .classList.add("active");
       });
+      // active section for animation on scroll
+      sec.classList.add("show-animate");
+    }
+    // If want to use animation that repeat on scroll use this
+    else {
+      sec.classList.remove("show-animate");
     }
   });
 
@@ -39,4 +45,23 @@ window.onscroll = () => {
   // remove toggle icon and navbar when click navbar links (scroll)
   menuIcon.classList.remove("bx-x");
   navbar.classList.remove("active");
+
+  // animation footer on scroll
+  window.addEventListener("scroll", function () {
+    let footer = document.querySelector("footer");
+
+    footer.classList.toggle(
+      "show-animate",
+      this.innerHeight + this.scrollY >= document.scrollingElement.scrollHeight
+    );
+  });
 };
+// Disable the keys
+document.addEventListener("contextmenu", function (e) {
+  e.preventDefault();
+});
+document.addEventListener("keydown", function (e) {
+  if (/*e.key === "F12" ||*/ e.ctrlKey && e.key === "u") {
+    e.preventDefault();
+  }
+});
