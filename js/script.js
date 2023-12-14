65b15fadaab3b4e27574dadd5d827fde28bad9cd
nameis-uttam <uttamjena343@gmail.com>
2023-12-14 21:10:27 +0530

    email module completed.

--- js/script.js
+++ js/script.js
@@ -56,12 +56,105 @@ window.onscroll = () => {
     );
   });
 };
+
+//Send message to email
+const form = document.querySelector("form");
+const fullName = document.getElementById("fullName");
+const emailId = document.getElementById("emailId");
+const mobile = document.getElementById("mobile");
+const subject = document.getElementById("subject");
+const message = document.getElementById("message");
+
+function sendEmail() {
+  const bodyMessage = `Full Name: ${fullName.value}<br> Email: ${emailId.value}<br> Mobile Number: ${mobile.value}<br> Message: ${message.value}`;
+  Email.send({
+    Host: "smtp.elasticemail.com",
+    Username: "uttamjena343@gmail.com",
+    Password: "EA2F538093C7149E0A4CC8FF524106E1491C",
+    To: "uttamjena343@gmail.com",
+    From: "uttamjena343@gmail.com",
+    Subject: subject.value,
+    Body: bodyMessage,
+  }).then((message) => {
+    if (message == "OK") {
+      Swal.fire({
+        title: "Success!",
+        text: "Message sent successfully!",
+        icon: "success",
+      });
+    }
+  });
+}
+
+function checkInputs() {
+  const items = document.querySelectorAll(".item");
+
+  for (const item of items) {
+    if (item.value == "") {
+      item.classList.add("error");
+      item.parentElement.classList.add("error");
+    }
+
+    if (items[1].value != "") {
+      checkEmail();
+    }
+
+    items[1].addEventListener("keyup", () => {
+      checkEmail();
+    });
+
+    item.addEventListener("keyup", () => {
+      if (item.value != "") {
+        item.classList.remove("error");
+        item.parentElement.classList.remove("error");
+      } else {
+        item.classList.add("error");
+        item.parentElement.classList.add("error");
+      }
+    });
+  }
+}
+
+function checkEmail() {
+  const emailRegex = /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,3})(\.[a-z]{2,3})?$/;
+  const errorTxtEmail = document.querySelector(".error-txt.email");
+
+  if (!emailId.value.match(emailRegex)) {
+    emailId.classList.add("error");
+    emailId.parentElement.classList.add("error");
+
+    if (emailId.value != "") {
+      errorTxtEmail.innerText = "Invalid Email";
+    } else {
+      errorTxtEmail.innerText = "Email address can't be blank";
+    }
+  } else {
+    emailId.classList.remove("error");
+    emailId.parentElement.classList.remove("error");
+  }
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  checkInputs();
+
+  if (
+    !fullName.classList.contains("error") &&
+    !emailId.classList.contains("error") &&
+    !mobile.classList.contains("error") &&
+    !subject.classList.contains("error") &&
+    !message.classList.contains("error")
+  ) {
+    sendEmail();
+  }
+});
+
 // Disable the keys
 document.addEventListener("contextmenu", function (e) {
   e.preventDefault();
 });
 document.addEventListener("keydown", function (e) {
-  if (e.key === "F12" || (e.ctrlKey && e.key === "u")) {
+  if (/*e.key === "F12" ||*/ e.ctrlKey && e.key === "u") {
     e.preventDefault();
   }
 });
